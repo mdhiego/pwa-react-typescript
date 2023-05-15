85b31d11ae5b1beba6cf306539e48c371773d729
dhiego.andrade <dhiego.andrade@ndbim.eu>
2023-05-15 07:56:52 -0300

    refactor: code cleanup

--- src/Contracts/Responses/TrackResponse.cs
+++ src/Contracts/Responses/TrackResponse.cs
@@ -5,6 +5,8 @@ public sealed record TrackResponse
     public int Order { get; set; }
     public required string Title { get; set; }
     public string? Description { get; set; }
+    public TimeSpan Duration { get; set; }
+    public string? Author { get; set; }
     public string? ImagePath { get; set; }
     public string? AudioPath { get; set; }
     public CategoryResponse? Category { get; set; }
--- src/Server/Domain/Track.cs
+++ src/Server/Domain/Track.cs
@@ -6,6 +6,8 @@ public sealed record Track
 
     public required string Title { get; set; }
     public string? Description { get; set; }
+    public TimeSpan Duration { get; set; }
+    public string? Author { get; set; }
     public string? ImagePath { get; set; }
     public string? AudioPath { get; set; }
     public Category? Category { get; set; }
